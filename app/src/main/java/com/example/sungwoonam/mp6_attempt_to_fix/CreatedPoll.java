d9f8a1f7e977a9ac9599f0a6d12737bf85de293a
James Nam <jamesn2@illinois.edu>
2018-12-09 18:38:14 -0600

    Finished QR code generator, working on scanner

--- app/src/main/java/com/example/sungwoonam/mp6_attempt_to_fix/CreatedPoll.java
+++ app/src/main/java/com/example/sungwoonam/mp6_attempt_to_fix/CreatedPoll.java
@@ -1,27 +1,30 @@
 package com.example.sungwoonam.mp6_attempt_to_fix;
 
 import android.content.Intent;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 
-public class CreatedPoll extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class CreatedPoll extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView scanner;
+    private static int camFB = Camera.CameraInfo.CAMERA_FACING_BACK;
+    private boolean accepted = true;
+
+
+    protected void onClick(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.created_poll);
+        scanner = new ZXingScannerView(this);
+        setContentView(scanner);
+    }
 
-        Button returnToStart = (Button) findViewById(R.id.button_return);
-        returnToStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(CreatedPoll.this, NewScreen.class);
-                startActivity(intent);
-            }
-        });
+    @Override
+    public void onResume() {
+        super.onResume();
     }
 }
 
