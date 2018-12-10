261f014148369e86438515fc24b7b1fb3b28f31c
James Nam <jamesn2@illinois.edu>
2018-12-09 18:22:08 -0600

    Finished QR code generator, working on scanner

--- app/src/main/java/com/example/sungwoonam/mp6_attempt_to_fix/NewScreen.java
+++ app/src/main/java/com/example/sungwoonam/mp6_attempt_to_fix/NewScreen.java
@@ -2,14 +2,32 @@ package com.example.sungwoonam.mp6_attempt_to_fix;
 
 import android.app.AppComponentFactory;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class NewScreen extends AppCompatActivity {
     private static final String TAG = "NewScreen";
@@ -20,6 +38,12 @@ public class NewScreen extends AppCompatActivity {
 
         ImageButton backtoStart = (ImageButton) findViewById(R.id.returnBack);
         Button createPoll = (Button) findViewById(R.id.createfinishedPoll);
+        final EditText editText1 = (EditText) findViewById(R.id.editText);
+        final EditText editText2 = (EditText) findViewById(R.id.editText2);
+        final EditText editText3 = (EditText) findViewById(R.id.editText3);
+        final EditText editText4 = (EditText) findViewById(R.id.editText4);
+        final ImageView qrView = (ImageView) findViewById(R.id.qrView);
+
         backtoStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,10 +55,35 @@ public class NewScreen extends AppCompatActivity {
         createPoll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast finishedToast = Toast.makeText(getApplicationContext(), "THANKS BRO", Toast.LENGTH_LONG);
+                Toast finishedToast = Toast.makeText(getApplicationContext(), "Poll Created!", Toast.LENGTH_LONG);
                 finishedToast.show();
-                Intent intent = new Intent(NewScreen.this, CreatedPoll.class);
-                startActivity(intent);
+                String title = editText1.getText().toString();
+                String option1 = editText2.getText().toString();
+                String option2 = editText3.getText().toString();
+                String option3 = editText4.getText().toString();
+
+                Gson qrGSON = new Gson();
+                Map<String, Object> json = new HashMap<>();
+                json.put("title", title);
+                json.put("option1", option1);
+                json.put("option2", option2);
+                json.put("option3", option3);
+                String jString = qrGSON.toJsonTree(json).toString();
+
+
+
+                if (title != null && option1 != null) {
+                    MultiFormatWriter multiFormat = new MultiFormatWriter();
+
+                    try {
+                        BitMatrix bitMatrix = multiFormat.encode(jString, BarcodeFormat.QR_CODE, 500, 500);
+                        BarcodeEncoder barcode = new BarcodeEncoder();
+                        Bitmap bitmap = barcode.createBitmap(bitMatrix);
+                        qrView.setImageBitmap(bitmap);
+                    } catch (WriterException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
     }
