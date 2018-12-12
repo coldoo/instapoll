b48b0cf2f25b1f02a65618143e164606196d3916
James Nam <jamesn2@illinois.edu>
2018-12-11 20:53:56 -0600

    final final

--- app/src/main/java/com/example/sungwoonam/mp6_attempt_to_fix/CreatedPoll.java
+++ app/src/main/java/com/example/sungwoonam/mp6_attempt_to_fix/CreatedPoll.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 
-/**public class CreatedPoll extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+public class CreatedPoll extends AppCompatActivity {
     private ZXingScannerView scanner;
     private static int camFB = Camera.CameraInfo.CAMERA_FACING_BACK;
     private boolean accepted = true;
@@ -26,6 +28,67 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
     public void onResume() {
         super.onResume();
     }
-}
 
-**/
+    Button cbutton1;
+    Button cbutton2;
+    Button cbutton3;
+    TextView class1;
+    TextView class2;
+    TextView class3;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.created_poll);
+
+        cbutton1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String penCount = class1.getText().toString();
+                int count = Integer.parseInt(penCount);
+                count++;
+                class1.setText(String.valueOf(count));
+                cbutton1.setEnabled(false);
+                cbutton2.setEnabled(false);
+                cbutton3.setEnabled(false);
+                Intent intent = new Intent(CreatedPoll.this, MainActivity.class);
+                startActivity(intent);
+                Toast finishedToast = Toast.makeText(getApplicationContext(), "Thanks! Your vote has been recorded" , Toast.LENGTH_LONG);
+                finishedToast.show();
+            }
+        });
+
+        cbutton2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String penCount = class2.getText().toString();
+                int count = Integer.parseInt(penCount);
+                count++;
+                class2.setText(String.valueOf(count));
+                cbutton1.setEnabled(false);
+                cbutton2.setEnabled(false);
+                cbutton3.setEnabled(false);
+                Intent intent = new Intent(CreatedPoll.this, MainActivity.class);
+                startActivity(intent);
+                Toast finishedToast = Toast.makeText(getApplicationContext(), "Thanks! Your vote has been recorded" , Toast.LENGTH_LONG);
+                finishedToast.show();
+            }
+        });
+
+        cbutton3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String penCount = class3.getText().toString();
+                int count = Integer.parseInt(penCount);
+                count++;
+                class3.setText(String.valueOf(count));
+                cbutton1.setEnabled(false);
+                cbutton2.setEnabled(false);
+                cbutton3.setEnabled(false);
+                Intent intent = new Intent(CreatedPoll.this, MainActivity.class);
+                startActivity(intent);
+                Toast finishedToast = Toast.makeText(getApplicationContext(), "Thanks! Your vote has been recorded" , Toast.LENGTH_LONG);
+                finishedToast.show();
+            }
+        });
+
+    }
+
+}
--- app/src/main/java/com/example/sungwoonam/mp6_attempt_to_fix/MainActivity.java
+++ app/src/main/java/com/example/sungwoonam/mp6_attempt_to_fix/MainActivity.java
@@ -7,8 +7,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -17,9 +19,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate: Starting.");
+        //Log.d(TAG, "onCreate: Starting.");
 
         Button switchActivity = (Button) findViewById(R.id.movetoNew);
+
         final Button scanQRCode = (Button) findViewById(R.id.scanPoll);
         final Activity activityForQR = this;
         switchActivity.setOnClickListener(new View.OnClickListener() {
@@ -35,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
                 IntentIntegrator scanIntegrator = new IntentIntegrator(activityForQR);
                 scanIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
                 scanIntegrator.setCameraId(0);
-                scanIntegrator.setPrompt("Scan QR");
                 scanIntegrator.setBarcodeImageEnabled(false);
                 scanIntegrator.setBeepEnabled(false);
                 scanIntegrator.setOrientationLocked(true);
@@ -43,5 +45,25 @@ public class MainActivity extends AppCompatActivity {
                 scanIntegrator.initiateScan();
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Scanning Cancelled", Toast.LENGTH_LONG).show();
+
+            }
+            else {
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+
+
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
